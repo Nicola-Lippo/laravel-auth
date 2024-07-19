c276e962c129265e743f4ba2a9b3f8e6f54e3f11
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-19 17:20:41 +0200

    [UPD] popolazione tabella faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,33 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+//importo faker + alias
+use Faker\Generator as Faker;
+//slug per url
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    //passo la classe Faker al metodo run
+    public function run(Faker $faker): void
     {
-        //
+        //ciclo for
+        for ($i = 0; $i < 50; $i++) {
+            //new modello, in alto si importa
+            $project = new Project();
+            //asswgna valori
+            $project->title = $faker->sentence(5);
+            $project->description = $faker->text(25);
+            //i dati sono gia pronti, save consolida
+            $project->slug = Str::of($project->title)->slug('-');
+            //consolida i dati
+            $project->save();
+        }
     }
 }
