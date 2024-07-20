93089896bc6f547a66d7bb5666399ee65ec1606a
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-20 12:47:29 +0200

    [UPD] icons + revisione layouts

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <div class="container">
         <h2 class="text-center py-3">Sezione Progetti Admin (S.P.A.)</h2>
-        <section>
+        <div class="d-flex justify-content-center">
+            <button class="btn btn-primary btn-lg">Crea un nuovo progetto</button>
+        </div>
+        <section class="py-5">
             <div class="d-flex">
                 <div class="row">
                     @foreach ($project as $token)
@@ -15,7 +18,17 @@
                                     <p><strong>SLUG:</strong> {{ $token->slug }}</p>
                                     <h4>ID: {{ $token->id }}</h4>
                                     <div class="card-footer">
-                                        <a href="#" class="btn btn-primary">Go</a>
+                                        <div class="d-flex justify-content-around py-3">
+                                            <a href="#" class="btn btn-primary">
+                                                <i class="fa-solid fa-magnifying-glass"></i>
+                                            </a>
+                                            <a href="#" class="btn btn-warning">
+                                                <i class="fa-solid fa-pen"></i>
+                                            </a>
+                                            <a href="#" class="btn btn-danger">
+                                                <i class="fa-solid fa-trash"></i>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
